98c5f1e3f2ed358bbefc36b3c75d099eb5bcbd41
ckh-kwanhao <kwanhao.chan@gmail.com>
2024-04-30 07:18:26 +0000

    Included teacher model saving in run_kd.py; revised eval.py where user directly indicate multiple eval_task there instead of in *run.sh for ICL training - retry :(

--- experiments/in_context/run.sh
+++ experiments/in_context/run.sh
@@ -10,20 +10,20 @@ source $PROJECT_DIR/scripts/misc/setup.sh
 export NCCL_DEBUG=INFO
 
 
-bash $PROJECT_DIR/scripts/in_context/mnli/run_minimal.sh qqp 64 facebook/opt-125m 1 30000
-bash $PROJECT_DIR/scripts/in_context/mnli/run_gpt3.sh qqp 64 facebook/opt-125m 1 30000
-bash $PROJECT_DIR/scripts/in_context/mnli/run_eval_harness.sh qqp 64 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_minimal.sh mnli 64 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_gpt3.sh mnli 64 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_eval_harness.sh mnli 64 facebook/opt-125m 1 30000
 
-bash $PROJECT_DIR/scripts/in_context/mnli/run_minimal.sh qqp 32 facebook/opt-125m 1 30000
-bash $PROJECT_DIR/scripts/in_context/mnli/run_gpt3.sh qqp 32 facebook/opt-125m 1 30000
-bash $PROJECT_DIR/scripts/in_context/mnli/run_eval_harness.sh qqp 32 facebook/opt-125m 1 30000
-
-
-bash $PROJECT_DIR/scripts/in_context/mnli/run_minimal.sh qqp 16 facebook/opt-125m 1 30000
-bash $PROJECT_DIR/scripts/in_context/mnli/run_gpt3.sh qqp 16 facebook/opt-125m 1 30000
-bash $PROJECT_DIR/scripts/in_context/mnli/run_eval_harness.sh qqp 16 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_minimal.sh mnli 32 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_gpt3.sh mnli 32 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_eval_harness.sh mnli 32 facebook/opt-125m 1 30000
 
 
-bash $PROJECT_DIR/scripts/in_context/mnli/run_minimal.sh qqp 2 facebook/opt-125m 1 30000
-bash $PROJECT_DIR/scripts/in_context/mnli/run_gpt3.sh qqp 2 facebook/opt-125m 1 30000
-bash $PROJECT_DIR/scripts/in_context/mnli/run_eval_harness.sh qqp 2 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_minimal.sh mnli 16 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_gpt3.sh mnli 16 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_eval_harness.sh mnli 16 facebook/opt-125m 1 30000
+
+
+bash $PROJECT_DIR/scripts/in_context/mnli/run_minimal.sh mnli 2 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_gpt3.sh mnli 2 facebook/opt-125m 1 30000
+bash $PROJECT_DIR/scripts/in_context/mnli/run_eval_harness.sh mnli 2 facebook/opt-125m 1 30000
